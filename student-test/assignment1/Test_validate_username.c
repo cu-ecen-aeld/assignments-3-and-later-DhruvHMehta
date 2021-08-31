311cec8e7c63784dec56bb07a5c834b38b0451a6
Dhruv Mehta <77941477+DhruvHMehta@users.noreply.github.com>
2021-08-30 20:42:55 -0600

    Modified username validation to free pointer

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,6 +18,10 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-	TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), malloc_username_from_conf_file(), "Username validation failed");
+	char *username_from_conf_file = malloc_username_from_conf_file();
+
+	TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), username_from_conf_file, "Username validation failed");
+
+	free(username_from_conf_file);
     //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 }
